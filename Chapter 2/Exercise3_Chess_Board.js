085f57330cfe2ef0a07daeafc5f416937cc03b42
Sam Friedman <friedman27@users.noreply.github.com>
2018-01-21 22:38:16 -0800

    Updated to include size variable
    Added size variable allows this program to change the size of the chess board to match the length and height based on the size variable without modifying the code otherwise.

--- Chapter 2/Exercise3_Chess_Board.js
+++ Chapter 2/Exercise3_Chess_Board.js
@@ -1,7 +1,11 @@
-for (var i = 0; i < 8; i++) {
+// can change both the length and width of chess board using the size variable
+var size = 8;
+var output = "";
+for (var i = 0; i < size; i++) {
   if(i % 2 === 0 || i === 0) {
-    console.log(" # # # #");
+    output = " #"
   } else {
-    console.log("# # # # ");
+    output = "# ";
   }
+  console.log(output.repeat(size/2));
 }
