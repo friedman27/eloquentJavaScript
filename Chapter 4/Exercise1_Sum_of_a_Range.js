0d2ab4cb9286a103ac281b7efabed7315f6b46bd
Sam Friedman <friedman27@users.noreply.github.com>
2018-01-22 21:23:00 -0800

    Update Exercise1_Sum_of_a_Range.js

--- Chapter 4/Exercise1_Sum_of_a_Range.js
+++ Chapter 4/Exercise1_Sum_of_a_Range.js
@@ -7,12 +7,6 @@ Next, write a sum function that takes an array of numbers and returns
 the sum of these numbers. Run the previous program and see whether it
 does indeed return 55.
 
-As a bonus assignment, modify your range function to take an optional
-third argument that indicates the “step” value used to build up the array.
-If no step is given, the array elements go up by increments of one,
-corresponding to the old behavior. The function call range(1, 10, 2) should
-return [1, 3, 5, 7, 9]. Make sure it also works with negative step values
-so that range(5, 2, -1) produces [5, 4, 3, 2].
 */
 
 // takes two arguments and returns an array of all the numbers from start up to (and including) end
